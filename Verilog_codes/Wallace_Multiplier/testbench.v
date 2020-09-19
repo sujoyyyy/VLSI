44b0c4261ac7f665093e25fb839114a637a680a8
Sujoy Datta <sujoydatta.2009@gmail.com>
2020-09-19 15:49:59 +0530

    changes

---
+++ Verilog_codes/Wallace_Multiplier/testbench.v
@@ -0,0 +1,17 @@
+`include "wallace.v"
+
+module tbench;
+
+reg [31:0] a, b;
+wire [63:0]out;
+
+wallace test (a, b, out);
+
+initial
+begin
+	a = 32'd1289;
+	b = 32'd11;
+	#10$display("  a=%d\n  b=%d\nout=%d", a, b, out);
+end
+
+endmodule
